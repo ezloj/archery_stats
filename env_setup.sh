976ee31bcb161ef9b44d994e4ec4ceb72d8c8227
ZLOJ <jan.lutenko@gmail.com>
2017-10-13 17:00:33 +0200

    Repository initial layout and env setup

---
+++ env_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+# I have my Python installed here right now.
+# On windows you have to insert pip to PATH, else it won't work
+# It's not there after installation. Whatevs, hardcode ftw
+PATH+=":/e/Python/Scripts"
+pip install -r requirements.txt
